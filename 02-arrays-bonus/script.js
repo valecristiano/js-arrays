9170a83ccbc19a1fab4a5f78f5bff06522943313
valecristiano <valeriamdcristiano@gmail.com>
2025-12-18 19:45:13 +0300

    bonus 2-4

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -5,24 +5,47 @@ const teachers = ["Nathan", "Ed", "Fabio", "Phil", "Carlo", "Lewis", "Luca"]; //
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
+const longNames = [];
 for (let i = 0; i < teachers.length; i++) {
-  const singleteacher = teachers[i];
+  const currentteacher = teachers[i];
 
-  const teachernames = singleteacher.length >= 5;
+  const teachernames = currentteacher.length >= 5;
 
   if (teachernames) {
-    const longNames = singleteacher;
-    console.log(longNames);
+    longNames.push(teachers[i]);
   }
 }
+console.table(longNames);
 
 // 3. Rimuovi 'Ed' dall'array teachers
-teachers.splice(1, 1);
-console.table(teachers);
+const edout = teachers.splice(1, 1);
+console.table(edout);
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-const isFabioPresent = null;
+
+let fabiodata = false;
+const searchedteacher = "Fabio";
+
+for (let i = 0; i < teachers.length; i++) {
+  const currentteacher = teachers[i];
+  // console.log(currentteacher);
+  // console.log(searchedteacher);
+  // console.log(currentteacher == searchedteacher);
+
+  if (currentteacher == searchedteacher) {
+    fabiodata = true;
+  }
+}
+
+const isFabioPresent = fabiodata;
+console.log("isFabioPresent", isFabioPresent);
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
-const teachersString = null;
+let teachersString = "";
+
+for (let i = 0; i < teachers.length; i++) {
+  const currentteacher = teachers[i];
+  teachersString += currentteacher;
+  console.log(teachersString);
+}
